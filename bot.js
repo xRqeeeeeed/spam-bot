8c8a0b7713294d529ba80016338c3fd6d6a8a45b
xRqeeeeeed <46226382+xRqeeeeeed@users.noreply.github.com>
2018-12-29 22:16:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client4 = new Discord.Client();
 const client5 = new Discord.Client();
 const client6 = new Discord.Client();
 const client7 = new Discord.Client();
-
+const client8 = new Discord.Client();
 
 client.on('ready', async() => {
 var server = "523298781857579009"; // ايدي السررفر
@@ -58,6 +58,13 @@ var channel = "523534835910967306";//ايدي الروم
     },305);
 })
 
+client8.on('ready', async() => {
+var server = "523298781857579009"; // ايدي السررفر
+var channel = "523534835910967306";//ايدي الروم
+    setInterval(()=>{
+    client8.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    },305);
+})
 
 
 client.login(process.env.BOT_TOKEN);
@@ -67,4 +74,4 @@ client4.login(process.env.BOT_TOKEN4);
 client5.login(process.env.BOT_TOKEN5);
 client6.login(process.env.BOT_TOKEN6);
 client7.login(process.env.BOT_TOKEN7);
-
+client8.login(process.env.BOT_TOKEN8);
